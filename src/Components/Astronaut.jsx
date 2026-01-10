5cca352038c6e22e641c184766beda1a8a060484
hetuk2005 <patelhetuk2005@gmail.com>
2026-01-10 23:17:11 +0530

    Code

--- src/Components/Astronaut.jsx
+++ src/Components/Astronaut.jsx
@@ -2,24 +2,34 @@ import React, { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import { useGLTF } from "@react-three/drei";
 
-export const Astronaut = () => {
+export const Astronaut = (onClick) => {
   const ref = useRef();
   const { scene } = useGLTF("/astronaut.glb");
+  const baseY = -1;
 
   // Slow Floating Rotation
-  useFrame(() => {
-    if (ref.current) {
-      ref.current.rotation.y += 0.003;
-    }
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+
+    // Floating Up & Down
+    ref.current.position.y = baseY + Math.sin(time) * 0.3;
+
+    // Slow Rotation
+    ref.current.rotation.y += 0.003;
+
+    // Mouse Based Rotation
+    ref.current.rotation.x = state.mouse.y * 0.4;
+    ref.current.rotation.z = state.mouse.x * 0.4;
   });
 
   return (
     <primitive
       ref={ref}
       object={scene}
-      scale={1.5}
-      position={[0, -1.5, 0]}
+      scale={0.8}
+      position={[0, -1.1, 0]}
       rotation={[0, Math.PI, 0]}
+      onClick={onClick}
     />
   );
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stars } from "@react-three/drei";
 import { Astronaut } from "../Components/Astronaut";
 
 export const Home = () => {
+  const cameraRef = useRef();
+  const [showProfile, setShowProfile] = useState(false);
+
   return (
     <div style={{ height: "100vh", background: "#000" }}>
-      <Canvas camera={{ position: [0, 1, 8], fov: 45 }}>
+      {showProfile && (
+        <div>
+          <img src="../../public/My_Image.jpg" alt="My Image" />
+          <h2>HETUK PATEL</h2>
+          <p>Fronted Developer</p>
+        </div>
+      )}
+      <Canvas
+        camera={{ position: [0, 1, 7], fov: 45 }}
+        onCreated={({ camera }) => (cameraRef.current = camera)}
+      >
         {/* Lights */}
         <ambientLight intensity={0.5} />
         <directionalLight position={[5, 5, 5]} intensity={1} />
 
         {/* Space Background */}
-        <Stars radius={100} depth={50} count={5000} factor={5} />
+        <Stars radius={100} depth={50} count={7000} factor={5} speed={1} />
 
         {/* Astronaut */}
-        <Astronaut />
+        <Astronaut onClick={() => setShowProfile(true)} />
 
         {/* Mouse Controls */}
-        <OrbitControls enableZoom={true} />
+        <OrbitControls enableZoom={true} minDistance={4} maxDistance={10} />
       </Canvas>
     </div>
   );
