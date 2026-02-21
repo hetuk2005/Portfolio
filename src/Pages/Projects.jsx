51dafdc4764f7f0d7f646de2cc40636666a1d19f
hetuk2005 <patelhetuk2005@gmail.com>
2026-02-21 19:38:21 +0530

    Code

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -25,7 +25,11 @@ export const Projects = ({ setPage }) => {
               <img src="/anime_Website_Screenshot.png" alt="Website 1" />
               <div className="card_info">
                 <h1>Hetuk Anime Website</h1>
-                <h3>Description</h3>
+                <h3>
+                  Description: The Anime Website project showcases <br /> anime
+                  series and information in a visually appealing <br /> format
+                  for fans to explore and enjoy.
+                </h3>
                 <button className="card_btn">View Project</button>
               </div>
             </div>
@@ -38,7 +42,11 @@ export const Projects = ({ setPage }) => {
               <img src="/chai_Point_Screenshot.png" alt="Website 2" />
               <div className="card_info">
                 <h1>Hetuk Chai Point Website</h1>
-                <h3>Description</h3>
+                <h3>
+                  Description: The Hetuk Chai Point website is a <br />
+                  creative tea point portfolio project, likely designed to
+                  <br /> present tea products with a modern web interface.
+                </h3>
                 <button className="card_btn">View Project</button>
               </div>
             </div>
@@ -51,7 +59,14 @@ export const Projects = ({ setPage }) => {
               <img src="hetuk_restaurant.png" alt="Website 2" />
               <div className="card_info">
                 <h1>Hetuk Restaurant Website</h1>
-                <h3>Description</h3>
+                <h3>
+                  Description: The Hetuk Restaurant Website showcases a<br />
+                  premium fine-dining experience with an <br />
+                  elegant interface and rich visual presentation. <br />
+                  It allows users to explore curated <br />
+                  menus, filter dishes seamlessly, and enjoy a smooth, <br />
+                  real-world restaurant browsing experience.
+                </h3>
                 <button className="card_btn">View Project</button>
               </div>
             </div>
